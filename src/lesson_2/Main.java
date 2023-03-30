0be0c20cd6940fc60e288783318de6d2924bcd7e
Chilliets <matdol@mail.ru>
2023-03-30 23:05:32 +1100

    30.03.23

---
+++ src/lesson_2/Main.java
@@ -0,0 +1,42 @@
+package lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+        // ТИП кличка_переменной = значение которое мы хотим ей присвоить;
+//        byte x = 50;
+//        System.out.println(x);
+//        char b = 64;
+//        System.out.println(b);
+//        System.out.println("Буква " + b);
+//        boolean y = true;
+
+        byte a = 60;
+        byte b = 45;
+        byte c = 17;
+        int s = a + b + c;
+        System.out.println(s);
+
+        int hungrytime = 9;
+        int fulltime = 15;
+        int bananas = 3;
+        int hungryBananasPerMin = hungrytime / bananas;
+        int fullBananadPerMin = fulltime / bananas;
+        int diff = fullBananadPerMin - hungryBananasPerMin;
+        System.out.println("The difference is " + diff);
+
+//        boolean cond;
+//        int red = 10;
+//        int green = 5;
+//       boolean cond = ==
+//        System.out.println();
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/lesson_2/hw/hw.java
@@ -0,0 +1,53 @@
+package lesson_2.hw;public class hw {
+    public static void main(String[] args) {
+        double coin = 5;
+        double coinbox = 2;
+        double change = coin / coinbox;
+        System.out.println("Amount of money in each coinbox is " + change);
+
+        int a = 50;
+        int b = 20;
+        int s = a * b;
+        System.out.println("The area of the rectangle is " + s);
+
+        int hunTime = 9;
+        int fullTime = 15;
+        int banana = 3;
+        int qntBananaHunT = hunTime / banana;
+        int qntBananaFullT = fullTime / banana;
+        int diffOfNumbers = qntBananaFullT - qntBananaHunT;
+        System.out.println("The difference in the number of bananas is " + diffOfNumbers);
+
+        int a = 3;
+//        int a = 10;
+        int b = 6;
+//        int b = 12;
+//        int sum = 1 + a + b;
+//        int sum = 2 + a + b;
+        int sum = 3 + a + b;
+//        int sum = 4 + a + b;
+
+        System.out.println("sum = " + sum);
+
+        int a = 1;
+        int b = 3;
+        int c = 9;
+        int d = 27;
+
+        int result = -a + b - c + d;
+
+        System.out.println(result);
+
+
+
+
+
+
+
+
+
+    }
+
+
+    }
+
