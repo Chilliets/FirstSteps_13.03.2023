a268e84a1e7840fd2908a77e1eed232610c9a31a
Chilliets <matdol@mail.ru>
2023-04-02 01:14:20 +1100

    02.04.23 Циклы

---
+++ src/lesson_4/Main.java
@@ -0,0 +1,77 @@
+package lesson_4;
+
+public class Main {
+    public static void main(String[] args) {
+        // for i
+
+        // Первая часть - Объявление перемменной i - означает нижний порог от которого мы будем отсчитывать
+        // Вторая часть - Условие выхода из цикла
+        // Третья часть - Инкрементирование переменной
+
+//        for (int i = 0; i <= 5; i++) {
+//        System.out.println("Hello world!" + i);}
+
+//        for (int i = 1; i <= 100; i++)
+//        {System.out.println( i );}
+
+//        for (int i = 2; i <= 100; i = i + 2)
+//        {System.out.println(i);}
+
+//        for (int i = 1; i <= 100; i++)
+//        {if (i % 2 == 0) System.out.println(i);}
+
+//        for (int i = 50; i <= 150; i++)
+//        {if (i % 2 == 1) System.out.println(i);}
+
+//        for (int i = 51; i <= 149; i = i + 2)
+//        {System.out.println(i);}
+
+//        for (int i = 0; i <= 150; i++)
+//        {if (i % 3 == 0 && i % 5 == 0 && i != 0) System.out.println(i);}
+
+//        int sum = 0;
+//        for (int i = 10; i <= 20; i++)
+//        {sum = sum + i;}
+//        System.out.println(sum);
+
+//        int count = 0;
+//        for (int i = 10; i <= 50; i = i + 2)
+//        {count += i;}
+//        System.out.println(count);
+
+//        int count = 0;
+//        for (int i = 10; i <= 50; i++)
+//        {if (i % 2 == 0)
+//        {count += i;}
+//        } System.out.println(count);
+
+
+//        for (int i = 10; i <= 40; i++)
+//        if (i < 15 || i > 20) System.out.println(i);
+
+//        int i = 10;
+//        System.out.println("Final countdown... ");
+//        while (i > 0) { Thread.sleep(1500);
+//            System.out.println(i);
+//            i--;        }
+//        System.out.println("BOOOM");
+
+        int i = 0;
+        System.out.println("Start");
+
+        while (i <= 20) {if (i < 10 || i > 15)
+            System.out.println(i);
+            i++;}
+        System.out.println("Finish");
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/lesson_4/hw/hw.java
@@ -0,0 +1,49 @@
+package lesson_4.hw;
+
+import java.util.Scanner;
+
+public class hw {
+    public static void main(String[] args) {
+        /**Ввести с клавиатуры два числа m и n.
+         Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.*/
+
+//        Scanner scan = new Scanner(System.in);
+//        int m = scan.nextInt();
+//        int n = scan.nextInt();
+//        for (int i = 0; i < m; i++) {for (int j = 0; j < n; j++)
+//        {System.out.print("8");}
+//            System.out.println();}
+
+        /**Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.*/
+
+//        int a = 11;
+//        int b = 0;
+//        for (int i = 0; i < a; i++) {for (int j = 0; j < b; j++)
+//        {System.out.print("8");}
+//            b++;
+//            System.out.println();}
+
+        /**Ввести с клавиатуры три числа, вывести на экран среднее из них.
+         Т.е. не самое большое и не самое маленькое.
+         Если все числа равны, вывести любое из них.*/
+
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        int b = scan.nextInt();
+//        int c = scan.nextInt();
+//        if ((a < b && a > c) || (a < c && a > b)) {System.out.println(a);}
+//        else if ((b < a && b > c) || (b > a && b < c)) {System.out.println(b);}
+//        else if ((c < b && c > a) || (c > b && c < a)) {System.out.println(c);}
+
+        /**Вывести на экран таблицу умножения 10х10 используя цикл while.
+         Числа разделить пробелом.*/
+
+
+
+
+
+
+    }
+
+
+}
