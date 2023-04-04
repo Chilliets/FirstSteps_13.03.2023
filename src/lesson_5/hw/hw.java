4de382ccc88f1056e1e54eb498903ffa32a1ad07
Chilliets <matdol@mail.ru>
2023-04-05 01:31:30 +1100

    05.04.23 Массивы + HW

--- src/lesson_5/hw/hw.java
+++ src/lesson_5/hw/hw.java
@@ -14,13 +14,13 @@ public class hw {
          2. Ввести с клавиатуры 8 строк и сохранить их в массив.
          3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке. Каждый элемент - с новой строки.*/
 
-        int[] mas = new int[10];
-        for (int i = 0; i < 8; i++) {
-            mas[i] = scan.nextInt();
-        }
-        for (int i = mas.length - 1; i >= 0; i--) {
-            System.out.println(mas[i]);
-        }
+//        String[] array = new String[10];
+//        for (int i = 0; i < 8; i++) {
+//            array[i] = scan.next();
+//        }
+//        for (int i = array.length - 1; i >= 0; i--) {
+//            System.out.println(array[i]);
+//        }
 
         System.out.println("-----------------------------------------------------");
         System.out.println("TASK 2");
@@ -30,8 +30,110 @@ public class hw {
          1.2. Считай с консоли 10 чисел и заполни ими массив
          2. Найти максимальное число из элементов массива.*/
 
+//        int[] array1 = new int[10];
+//        for (int n = 0; n < array1.length; n++) {
+//            array1[n] = scan.nextInt();
+//        }
+//        int max = array1[0];
+//        for (int n = 0; n < array1.length; n++) {
+//            if (array1[n] > max)
+//                max = array1[n];
+//        }
+//        System.out.println(max);
+
+        System.out.println("-----------------------------------------------------");
+        System.out.println("TASK 3");
+
+        /**
+         1. Создать массив на 10 строк.
+         2. Создать массив на 10 чисел.
+         3. Ввести с клавиатуры 10 строк, заполнить ими массив строк.
+         4. В каждую ячейку массива чисел записать длину строки из массива строк,
+         индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.*/
+
+//        String[] word = new String[10];
+//
+//        for (int i = 0; i < word.length; i++) {
+//            word[i] = scan.nextLine();
+//        }
+//
+//        int[] number = new int[10];
+//        int a = word.length;{
+//
+//        for (int j = 0; j < number.length; j++)
+//            if (int a = number[j]);
+//
+//            number[j] = word.length;
+//
+//            System.out.println(number[j]);}
+
+//        String[] letters = new String[10];
+//        int[] numbers = new int[10];
+//
+//
+//        for (int i = 0; i < letters.length; i++)
+//            letters[i] = scan.nextLine();
+//
+//        for (int i = 0; i < numbers.length; i++)
+//            numbers[i] = Integer.parseInt(scan.next());
+//
+//        for (int i = 0; i < numbers.length; i++)
+//            System.out.println(numbers[i]);
+
+        System.out.println("-----------------------------------------------------");
+        System.out.println("TASK 4");
+
+        /**
+         1. Создать массив на 10 чисел.
+         2. Ввести с клавиатуры 10 чисел и записать их в массив.
+         3. Вывести на экран элементы массива в обратном порядке, каждое значение выводить с новой строки..*/
+
+//        int array2 [] = new int[10];
+//        for (int i = 0; i < array2.length; i++) {
+//            array2[i] = scan.nextInt();
+//
+//        }
+//        for (int i = array2.length - 1; i >= 0; i--) {
+//            System.out.println(array2[i]);
+//
+//        }
+
+        System.out.println("-----------------------------------------------------");
+        System.out.println("TASK 5");
+
+        /**
+         1. Создать массив на 20 чисел.
+         2. Ввести в него значения с клавиатуры.
+         3. Создать два массива на 10 чисел каждый.
+         4. Скопировать большой массив в два маленьких: половину чисел в первый маленький, вторую половину во второй маленький.
+         5. Вывести второй маленький массив на экран, каждое значение выводить с новой строки.*/
+
+//        int[] all = new int[20];
+//        for (int i = 0; i < all.length; i++)
+//            all[i] = scan.nextInt();
+//
+//        int[] a = new int[10];
+//        int[] b = new int[10];
+//        for (int i = 0; i < 10; i++) {
+//            if (all[i] != a[i])
+//                a[i] = all[i];
+//        }
+//        for (int j = 0; j <= 10; j++) {
+//            if (all[j + 10] != b[j])
+//                b[j] = all[j + 10];
+//            System.out.println(b[j]);
+//        }
+
+        System.out.println("-----------------------------------------------------");
+        System.out.println("TASK 6");
+
 
 
 
     }
+
 }
+
+
+
+
